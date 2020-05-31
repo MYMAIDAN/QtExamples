e1f50206e9a1442a8b58ce964da9b2e2b2a9915f
MYMAIDAN <mykhailo.maidan>
2020-05-31 17:39:48 +0300

    Update main exakmple

--- TreeModelView/Sources/main.cpp
+++ TreeModelView/Sources/main.cpp
@@ -10,6 +10,31 @@
 #include <QDirIterator>
 #include <QDir>
 
+void parseDir(QString dir, QModelIndex& parent, TreeModel& model)
+{
+  QDir qwe(dir);
+  qwe.setFilter(  QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot);
+  qwe.setSorting(QDir::Size | QDir::Reversed);
+  auto list2 = qwe.entryInfoList();
+  int j = 0;
+
+  for( auto it: list2 )
+  {
+    if( it.isDir() )
+    {
+        model.insertRows(j,1,parent);
+        model.setData(model.index(j,0,parent),it.fileName());
+        auto par = model.index(j++,0,parent);
+        parseDir( it.absoluteFilePath(),par,model);
+    }
+    if( it.isFile() )
+    {
+        model.insertRows(j,1,parent);
+        model.setData(model.index(j++,0,parent),it.fileName());
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
@@ -22,47 +47,14 @@ int main(int argc, char *argv[])
   file.open(QIODevice::ReadOnly);
   TreeModel *model = new TreeModel(headers, file.readAll());
   QDir ite("E:\\projects\\QtExamples\\TreeModelView\\");
+  ite.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks | QDir::NoDotAndDotDot);
+  ite.setSorting(QDir::Size | QDir::Reversed);
   auto list = ite.entryInfoList();
   model->insertRows( model->rowCount(), 1, model->index(model->rowCount(),0) );
   model->setData(model->index(model->rowCount()-1,0),ite.path());
   //model->insertColumn(0,model->index(model->rowCount()-1,0));
-
   auto parent = model->index(model->rowCount()-1,0);
-  int i = 0;
-  for( auto it: list )
-  {
-    if( it.isDir() )
-    {
-        auto in = parent.column();
-        model->insertRows(i,1,parent);
-        model->setData(model->index(i,0,model->index(model->rowCount()-1,0)),it.fileName());
-        QDir qwe(it.absoluteFilePath());
-        auto list2 = qwe.entryInfoList();
-        int j = 0;
-        auto par = model->index(i++,0,model->index(model->rowCount()-1,0));
-        for( auto it: list2 )
-        {
-          if( it.isDir() )
-          {
-              model->insertRows(j,1,par);
-              model->setData(model->index(j++,0,par),it.fileName());
-
-          }
-          if( it.isFile() )
-          {
-              model->insertRows(j,1,par);
-              model->setData(model->index(j++,0,par),it.fileName());
-          }
-        }
-
-    }
-    if( it.isFile() )
-    {
-        auto in = parent.column();
-        model->insertRows(i,1,parent);
-        model->setData(model->index(i++,0,model->index(model->rowCount()-1,0)),it.fileName());
-    }
-  }
+      parseDir("C:\\Users\\Mishs\\Downloads\\VA - Heavy Metal Collections Vol. 16 (3CD +1CD LIVE) - 2020, FLAC", parent, *model);
 
   file.close();
   v.setModel(model);
